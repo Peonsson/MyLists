52a2909cd238fb630fafc47cf968dcca75a1c184
Robin <robinvet@kth.se>
2016-06-13 18:55:49 +0200

    Add stuff

--- src/LinkedList/MyLinkedList.java
+++ src/LinkedList/MyLinkedList.java
@@ -1,23 +1,153 @@
 package LinkedList;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
-/**
- * Created by Xsnud on 2016-06-13.
- */
-public class MyLinkedList implements List, Iterable  {
-
+public class MyLinkedList<E> implements List<E> {
+    private Node<E> head;
     private int size;
-    private Node head;
-    private Node tail;
 
     public MyLinkedList() {
-        head = new Node();
-        tail = new Node();
+        head = null;
+        size = 0;
     }
+
+    public void add(int index, E item) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException(Integer.toString(index));
+        }
+
+        if (index == 0) {
+            addFirst(item);
+        }
+        else {
+            Node<E> node = getNode(index - 1);
+            addAfter(node, item);
+        }
+    }
+
+    // Returns null if node is missing
+    private Node<E> getNode(int index) {
+        Node<E> node = head;
+
+        for (int i = 0; i < index && node != null; i++) {
+            node = node.next;
+        }
+
+        return node;
+    }
+
+    private void addFirst(E item) {
+        head = new Node<E>(item, head);
+        size++;
+    }
+
+    private void addAfter(Node<E> node, E item) {
+        node.next = new Node<E>(item, node.next);
+        size++;
+    }
+
+    public boolean add(E item) {
+        add(size, item);
+        return true;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(Integer.toString(index));
+        }
+
+        Node<E> node = getNode(index);
+        return node.data;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(Integer.toString(index));
+        }
+
+        Node<E> nodeBefore;
+        E dataToReturn;
+
+        if (index == 0) {
+            dataToReturn = get(0);
+            head = getNode(1);
+        }
+        else {
+            nodeBefore = getNode(index - 1);
+            dataToReturn = get(index);
+
+            nodeBefore.next = nodeBefore.next.next;
+        }
+
+        size--;
+
+        return dataToReturn;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[ ");
+        Node<E> p = head;
+
+        if (p != null) {
+            while (p.next != null) {
+                sb.append(p.data.toString());
+                sb.append(" ==> ");
+
+                p = p.next;
+            }
+
+            sb.append(p.data.toString());
+        }
+
+        sb.append(" ]");
+        return sb.toString();
+    }
+
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public Iterator<E> iterator() {
+        return new Itr(head);
+    }
+
+    private class Itr implements Iterator<E> {
+        Node<E> current;
+
+        public Itr(Node<E> start) {
+            current = start;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public E next() {
+            if (current == null) {
+                throw new NoSuchElementException();
+            }
+
+            E returnValue = current.data;
+            current = current.next;
+
+            return returnValue;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
 
     @Override
     public int size() {
@@ -26,11 +156,7 @@ public class MyLinkedList implements List, Iterable  {
 
     @Override
     public boolean isEmpty() {
-
-        if(size == 0)
-            return true;
-
-        return false;
+        return size == 0;
     }
 
     @Override
@@ -39,27 +165,42 @@ public class MyLinkedList implements List, Iterable  {
     }
 
     @Override
-    public Iterator iterator() {
-        return null;
-    }
-
-    @Override
     public Object[] toArray() {
         return new Object[0];
     }
 
     @Override
-    public boolean add(Object o) {
-
-        Node newNode = new Node(o);
-        tail.setNext(newNode);
-        tail = tail.getNext();
-
-        return true;
+    public <T> T[] toArray(T[] a) {
+        return null;
     }
 
     @Override
     public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
         return false;
     }
 
@@ -69,22 +210,7 @@ public class MyLinkedList implements List, Iterable  {
     }
 
     @Override
-    public Object get(int index) {
-        return null;
-    }
-
-    @Override
-    public Object set(int index, Object element) {
-        return null;
-    }
-
-    @Override
-    public void add(int index, Object element) {
-
-    }
-
-    @Override
-    public Object remove(int index) {
+    public E set(int index, E element) {
         return null;
     }
 
@@ -99,50 +225,17 @@ public class MyLinkedList implements List, Iterable  {
     }
 
     @Override
-    public ListIterator listIterator() {
+    public ListIterator<E> listIterator() {
         return null;
     }
 
     @Override
-    public ListIterator listIterator(int index) {
+    public ListIterator<E> listIterator(int index) {
         return null;
     }
 
     @Override
-    public List subList(int fromIndex, int toIndex) {
+    public List<E> subList(int fromIndex, int toIndex) {
         return null;
-    }
-
-    @Override
-    public boolean containsAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public Object[] toArray(Object[] a) {
-        return new Object[0];
-    }
-
-    /*
-        Unsupported methods
-     */
-    @Override
-    public boolean retainAll(Collection c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean removeAll(Collection c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean addAll(Collection c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean addAll(int index, Collection c) {
-        throw new UnsupportedOperationException();
     }
 }
